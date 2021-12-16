a842351798d512e02f9b33bdb97b7393ee2ec73b
Martin Georgiev <mgeorgiev9611@abv.bg>
2021-12-16 16:39:56 +0000

    SECRET

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
+      secret: process.env.SECRET
     }),
     // ...add more providers here
   ],
