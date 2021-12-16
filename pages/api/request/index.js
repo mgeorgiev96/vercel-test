abecd3cbc1749d6b27d6b0527468087a174ee41c
Martin Georgiev <mgeorgiev9611@abv.bg>
2021-12-16 12:35:30 +0000

    Final

--- pages/api/request/index.js
+++ pages/api/request/index.js
@@ -3,17 +3,16 @@ const User = require('../../../models/model')
 const mongoose = require("mongoose")
 
 export default async function getData (req,res){
-    mongoose.disconnect()
-    // await mongoose.connect(process.env.MONGO_DB)
-    // await User.findOne({username:"martin"}).then(async user=>{
-    //     if(user){
-    //         res.send("already created")
-    //     }else{
-    //         await new User({
-    //             username: "markos",
-    //             password: "123"
-    //         }).save()
-    //     }
-    // }).catch(err=>console.log(err))
+    await mongoose.connect(process.env.MONGO_DB)
+    await User.findOne({username:"martin"}).then(async user=>{
+        if(user){
+            res.send("already created")
+        }else{
+            await new User({
+                username: "markos",
+                password: "123"
+            }).save()
+        }
+    }).catch(err=>console.log(err))
     res.end()
 }
